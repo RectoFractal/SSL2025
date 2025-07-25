08413b1a930d01c31ff6134504df00423ee721a0
RectoFractal <i.am.krull.tepesh@gmail.com>
2025-07-25 17:09:56 +0300

    Start write GK 2.0

--- bridge/strategy/strategy.py
+++ bridge/strategy/strategy.py
@@ -56,6 +56,40 @@ class Strategy:
                 self.run(field, actions)
 
         return actions
+    
+    def doPassNearAllly(field: fld.Field, actions: list[Action], idFrom = const.GK):
+        pass
+
+
+    def GK(field: fld.Field, actions: list[Action]):
+        field.allies[const.GK].set_dribbler_speed(0)
+
+        oldBallPos = field.ball_start_point
+        ballPos = field.ball.get_pos()
+        GKPos = field.allies[const.GK].get_pos()
+
+        nearestEnemyRToBall = fld.find_nearest_robot(ballPos, field.active_enemies())
+        enemyRGrabBall = field.is_ball_in(nearestEnemyRToBall)
+
+        if field.is_ball_moves_to_goal() and not enemyRGrabBall:
+            if not aux.is_point_on_line(GKPos, oldBallPos, ballPos, "R"):
+                """ intersept ball"""
+                interseptBallPoint = aux.closest_point_on_line(oldBallPos, ballPos, GKPos, "R")
+                actions[const.GK] = Actions.GoToPoint(interseptBallPoint)
+            else:
+                """grab intersepted ball"""
+                actions[const.GK] = Actions.BallGrab((ballPos-GKPos).arg)
+        elif field.is_ball_in(field.allies[const.GK]):
+            doPassNearAllly(field, actions)
+        else:
+            if enemyRGrabBall:
+                """block maybe kick"""
+                pointForGK = aux.nearest_point_in_poly(ballPos, field.ally_goal.hull)
+                actions[const.GK] = Actions.GoToPoint(pointForGK, (ballPos-GKPos).arg)
+                field.allies[const.GK].set_dribbler_speed(1)
+            # else:
+
+
 
     def run(self, field: fld.Field, actions: list[Optional[Action]]) -> None:
         """
@@ -81,31 +115,6 @@ class Strategy:
         - actions[9] = Actions.BallGrab(0.0)
                 The robot number 9 grabs the ball at an angle of 0.0 (it looks to the right, along the OX axis)
         """
-        # field.allies[const.GK].set_dribbler_speed(0)
-
-        # oldBallPos = field.ball_start_point
-        # ballPos = field.ball.get_pos()
-        # GKPos = field.allies[const.GK].get_pos()
-
-        # nearestEnemyRToBall = fld.find_nearest_robot(ballPos, field.active_enemies())
-        # enemyRGrabBall = field.is_ball_in(nearestEnemyRToBall)
-
-        # if field.is_ball_moves_to_goal() and not enemyRGrabBall:
-        #     if not aux.is_point_on_line(GKPos, oldBallPos, ballPos, "R"):
-        #         """ intersept ball"""
-        #         interseptBallPoint = aux.closest_point_on_line(oldBallPos, ballPos, GKPos, "R")
-        #         actions[const.GK] = Actions.GoToPoint(interseptBallPoint)
-        #     else:
-        #         """grab intersepted ball"""
-        #         actions[const.GK] = Actions.BallGrab((ballPos-GKPos).arg)
-        # else:
-        #     if enemyRGrabBall:
-        #         """block maybe kick"""
-        #         pointForGK = aux.nearest_point_in_poly(ballPos, field.ally_goal.hull)
-        #         actions[const.GK] = Actions.GoToPoint(pointForGK, (ballPos-GKPos).arg)
-        #         field.allies[const.GK].set_dribbler_speed(1)
-        #     else:
-
 
         # rPos = field.allies[const.GK]
         # ballPos = field.ball.get_pos()
